9ee5304ebb3e9e63c7e5a5ac9738f4f700c4edac
kojikawazu <nanairo7.daydream21@gmail.com>
2023-08-21 22:24:17 +0900

    hooks仮commit

---
+++ src/app/hooks/useScrol.tsx
@@ -0,0 +1,23 @@
+/**
+ * 先頭へスクロールするhook
+ */
+export const useScrolTop = () => {
+    window.scroll({ 
+        top: 0, 
+        behavior: 'smooth' 
+    });
+}
+
+/**
+ * コンポーネントへスクロール移動するhook
+ * @param refData React.RefObject<HTMLDivElement>
+ */
+export const useScrollToRef = (refData: React.RefObject<HTMLDivElement> | undefined | null) => {
+    return () => {
+        if (refData) {
+            refData.current?.scrollIntoView({
+                behavior: 'smooth' 
+            });
+        }
+    }
+};

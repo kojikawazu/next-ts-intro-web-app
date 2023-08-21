6943e5e1b136f334d85799e52a4e72760942ba8e
kojikawazu <nanairo7.daydream21@gmail.com>
2023-08-21 22:18:23 +0900

    初期設定

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        // 以下ドメインへアクセスすることを許可します。
+        domains: ['images.unsplash.com', 'plus.unsplash.com']
+    }
+}
+
 
 module.exports = nextConfig
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,15 +3,21 @@ import type { Config } from 'tailwindcss'
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'lblue': 'rgba(157, 237, 255, 0.70)',
+        'dblue': 'rgba(0, 121, 148, 1)',
+        'hero':  'rgba(173, 216, 230, 0.51)',
+        'career': '#84C1CE',
+        'skills': '#D3D3D3',
+        'footer': '#007994'
+      },
+      screens: {
+        xs: '300px',
       },
     },
   },

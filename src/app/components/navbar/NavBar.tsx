3b9fb957cc5cba16548ca1f892b28ec5ba83c9fa
kojikawazu <nanairo7.daydream21@gmail.com>
2023-08-21 22:26:05 +0900

    NavBarコンポーネント仮commit

---
+++ src/app/components/navbar/NavBar.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useIntroData } from '@/app/contexts/introContext';
+import { NavBarType } from '@/app/types/NavbarType';
+import { useScrolTop } from '@/app/hooks/useScrol';
+import HumbergerMenu from './HumbergerMenu';
+import NormalMenu from './NormalMenu';
+
+/**
+ * MavBarコンポーネント
+ * @returns JSX
+ */
+const NavBar= () => {
+    const { introData } = useIntroData();
+    const navBarData: NavBarType = introData!.navbar_data;
+
+    return (
+        <div className="flex justify-between items-center bg-lblue w-full h-[100px]">
+            <button 
+                className="text-sm xs:text-2xl sm:text-3xl pl-9 underline decoration-1 decoration-solid underline-offset-8"
+                onClick={useScrolTop}>
+                <h1>{navBarData.link_title}</h1>
+            </button>
+
+            <div className="flex justify-end items-center">
+                <div className="hidden md:inline-block">
+                    <NormalMenu />
+                </div>
+                <div className="inline-block md:hidden">
+                    <HumbergerMenu />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
---
+++ src/app/components/navbar/NormalMenu.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useIntroData } from '@/app/contexts/introContext';
+import { NavBarType } from '@/app/types/NavbarType';
+import { useScrollToRef } from '@/app/hooks/useScrol';
+
+/**
+ * ノーマルメニューコンポーネント
+ * @returns JSX
+ */
+const NormalMenu = () => {
+    // Context
+    const { introData, refData } = useIntroData();
+    const navBarData: NavBarType = introData!.navbar_data;
+
+    // hooks
+    const aboutScrolToRef = useScrollToRef(refData?.aboutRef);
+    const careerScrolToRef = useScrollToRef(refData?.careerRef);
+    const skillsScrolToRef = useScrollToRef(refData?.skillsRef);
+    const contactScrolToRef = useScrollToRef(refData?.contactRef);
+
+    return (
+        <>
+            <div className="flex mt-3 mr-3">
+                <div className="px-6 border-r border-black">
+                    <button onClick={aboutScrolToRef}>{navBarData.about_name}</button>
+                </div>
+                <div className="px-6 border-r border-black">
+                    <button onClick={careerScrolToRef}>{navBarData.career_name}</button>
+                </div>
+                <div className="px-6 border-r border-black">
+                    <button onClick={skillsScrolToRef}>{navBarData.skills_name}</button>
+                </div>
+                <div className="px-6">
+                    <button onClick={contactScrolToRef}>{navBarData.contact_name}</button>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default NormalMenu;

63e207c6b9ca0d1b27f2e98b8da75504b211a3ae
kojikawazu <nanairo7.daydream21@gmail.com>
2023-08-21 22:21:08 +0900

    型定義commit

---
+++ src/app/types/AboutType.ts
@@ -0,0 +1,9 @@
+/** About型定義 */
+export interface AboutType {
+    intro_name: string;
+    intro_icon_url: string;
+    intro_img_url: string;
+    intro_x_url: string;
+    intro_github_url: string; 
+    intro_contents: Array<string>;
+}
